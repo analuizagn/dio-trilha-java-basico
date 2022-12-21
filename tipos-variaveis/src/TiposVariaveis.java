cce4e8b41c9c053abbe4e11f1caa3c7a93148e8b
analuizagn <analuizagn@gmail.com>
2022-12-20 22:38:02 -0300

    adicionado novo projeto

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,10 +1,6 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
 
-        short numeroCurto = 1;
-        int numeroNormal = numeroCurto;
-        short numeroCurto2 = (short) numeroNormal;
-
         int numero = 1;
 
         numero = 10;
